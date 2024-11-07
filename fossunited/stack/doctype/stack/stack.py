26ccd0193c994567be52917546361fccb8a5c5d5
harshtandiya <harsh.tandiya@gmail.com>
2024-11-08 00:43:25 +0530

    chore(stack): better doctype permission

--- fossunited/stack/doctype/stack/stack.py
+++ fossunited/stack/doctype/stack/stack.py
@@ -6,4 +6,17 @@ from frappe.model.document import Document
 
 
 class Stack(Document):
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        category: DF.Data
+        icon: DF.AttachImage
+        name1: DF.Data
+    # end: auto-generated types
+
     pass

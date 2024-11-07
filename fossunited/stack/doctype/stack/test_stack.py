d6fcee895f8894b79456a6cf8c3e50c2cd18c190
harshtandiya <harsh.tandiya@gmail.com>
2024-11-07 12:39:25 +0530

    feat: add `Stack` doctype

---
+++ fossunited/stack/doctype/stack/test_stack.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2024, Frappe x FOSSUnited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase
+
+# On IntegrationTestCase, the doctype test records and all
+# link-field test record depdendencies are recursively loaded
+# Use these module variables to add/remove to/from that list
+EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
+
+
+class TestStack(IntegrationTestCase):
+    """
+    Integration tests for Stack.
+    Use this class for testing interactions between multiple components.
+    """
+
+    pass

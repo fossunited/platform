eaec3f6f5e07cf6dc3f6bc13c70a36ff18eb9a00
harshtandiya <harsh.tandiya@gmail.com>
2024-11-08 12:35:43 +0530

    chore(stack): use `collections.defaultdict`

--- fossunited/stack/utils.py
+++ fossunited/stack/utils.py
@@ -1,15 +1,14 @@
+from collections import defaultdict
+
 import frappe
 
 
 def get_stack_dict():
     stacks = frappe.get_all(doctype="Stack", fields=["name1", "icon", "category"], page_length=999)
 
-    stack_dict = {}
+    stack_dict = defaultdict(list)
 
     for item in stacks:
-        if item.category not in stack_dict:
-            stack_dict.update({item.category: []})
-
         stack_dict[item.category].append(item)
 
-    return stack_dict
+    return dict(stack_dict)

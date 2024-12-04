76d896a0b682fa1da426195aa280b6e3d0443cca
harshtandiya <harsh.tandiya@gmail.com>
2024-12-04 16:21:08 +0530

    chore: docstring for `NewsletterExtend` class

--- fossunited/overrides/newsletter_extend.py
+++ fossunited/overrides/newsletter_extend.py
@@ -4,6 +4,19 @@ from frappe.utils import random_string
 
 
 class NewsletterExtend(Newsletter):
+    """
+    An extended Newsletter class that generates unique identifiers for newsletter documents.
+
+    This class inherits from the base Newsletter class and overrides the naming mechanism
+    to ensure each newsletter has a unique identifier.
+
+    By default, the name of a newsletter is set to the subject, which can lead to conflicts.
+    This extended class addresses this issue by providing a custom naming scheme.
+
+    Attributes:
+        Inherits all attributes from the base Newsletter class.
+    """
+
     def autoname(self):
         unique_code = self.generate_unique_newsletter_code()
         self.name = unique_code

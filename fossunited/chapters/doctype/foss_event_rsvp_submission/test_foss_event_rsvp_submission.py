6b0c1fcd06c2146803fd23df4c74d82b06f61f59
Harsh Tandiya <harsh.tandiya@gmail.com>
2024-12-11 11:25:22 +0530

    Make tests better (#703)
    feat(test): add test utils file and refactor tests

--- fossunited/chapters/doctype/foss_event_rsvp_submission/test_foss_event_rsvp_submission.py
+++ fossunited/chapters/doctype/foss_event_rsvp_submission/test_foss_event_rsvp_submission.py
@@ -1,13 +1,14 @@
-# Copyright (c) 2023, Frappe x FOSSUnited and Contributors
-# See license.txt
-
-from datetime import datetime, timedelta
-
 import frappe
 from faker import Faker
 from frappe.tests import IntegrationTestCase
 
-from fossunited.doctype_ids import EVENT, EVENT_RSVP, RSVP_RESPONSE
+from fossunited.doctype_ids import CHAPTER, EVENT, EVENT_RSVP
+from fossunited.tests.utils import (
+    insert_rsvp_form,
+    insert_rsvp_submission,
+    insert_test_chapter,
+    insert_test_event,
+)
 
 fake = Faker()
 
@@ -16,53 +17,23 @@ WEBSITE_USER = "test2@example.com"
 
 class TestFOSSEventRSVPSubmission(IntegrationTestCase):
     def setUp(self):
-        event = frappe.get_doc(
-            {
-                "doctype": EVENT,
-                "event_name": fake.name(),
-                "event_permalink": fake.slug(),
-                "status": "Live",
-                "event_type": "FOSS Meetup",
-                "event_start_date": datetime.today(),
-                "event_end_date": datetime.today() + timedelta(1),
-                "event_description": "testing",
-            }
-        )
-        event.insert()
-        event.reload()
-        self.event = event
-
-        rsvp = frappe.get_doc(
-            {
-                "doctype": EVENT_RSVP,
-                "max_rsvp_count": 5,
-                "event": event.name,
-            }
-        )
-        rsvp.insert()
-        rsvp.reload()
-        self.rsvp = rsvp
+        self.chapter = insert_test_chapter()
+        self.event = insert_test_event(chapter=self.chapter)
+        self.rsvp = insert_rsvp_form(event=self.event.name)
 
     def tearDown(self):
-        frappe.delete_doc(EVENT, self.event.name, force=1)
-        frappe.delete_doc(EVENT_RSVP, self.rsvp.name, force=1)
+        frappe.set_user("Administrator")
+        frappe.delete_doc(CHAPTER, self.chapter.name, force=True)
+        frappe.delete_doc(EVENT, self.event.name, force=True)
+        frappe.delete_doc(EVENT_RSVP, self.rsvp.name, force=True)
 
     def test_unpublish_on_max_count(self):
         # Given an RSVP form with max count
         rsvp = self.rsvp
 
         # When submission count reaches the max count
-        for i in range(int(rsvp.max_rsvp_count)):
-            frappe.get_doc(
-                {
-                    "doctype": RSVP_RESPONSE,
-                    "linked_rsvp": rsvp.name,
-                    "event": rsvp.event,
-                    "name1": fake.name(),
-                    "email": fake.email(),
-                    "im_a": "Student",
-                }
-            ).insert()
+        for _ in range(int(rsvp.max_rsvp_count)):
+            insert_rsvp_submission(linked_rsvp=self.rsvp.name)
 
         # Then the RSVP must be unpublished
         is_published = frappe.db.get_value(EVENT_RSVP, rsvp.name, "is_published")
@@ -70,24 +41,13 @@ class TestFOSSEventRSVPSubmission(IntegrationTestCase):
 
     def test_add_to_email_group(self):
         # Given an RSVP form for an event
-        form = self.rsvp
-
-        frappe.set_user(WEBSITE_USER)
         # When an RSVP response is done by a user
-        frappe.get_doc(
-            {
-                "doctype": RSVP_RESPONSE,
-                "linked_rsvp": form.name,
-                "event": form.event,
-                "name1": fake.name(),
-                "email": WEBSITE_USER,
-                "im_a": "Student",
-            }
-        ).insert()
+        frappe.set_user(WEBSITE_USER)
+        insert_rsvp_submission(linked_rsvp=self.rsvp.name, email=WEBSITE_USER)
 
         # Then the email should be added to an email group linked to event for participants
         email_group = frappe.db.get_value(
-            "Email Group", {"event": form.event, "group_type": "Event Participants"}
+            "Email Group", {"event": self.rsvp.event, "group_type": "Event Participants"}
         )
 
         self.assertTrue(
@@ -95,5 +55,3 @@ class TestFOSSEventRSVPSubmission(IntegrationTestCase):
                 "Email Group Member", {"email": WEBSITE_USER, "email_group": email_group}
             )
         )
-
-        frappe.set_user("Administrator")
--- fossunited/ticketing/doctype/foss_event_ticket/test_foss_event_ticket.py
+++ fossunited/ticketing/doctype/foss_event_ticket/test_foss_event_ticket.py
@@ -1,74 +1,24 @@
-# Copyright (c) 2024, Frappe x FOSSUnited and Contributors
-# See license.txt
-
-from datetime import datetime, timedelta
-
 import frappe
 from faker import Faker
 from frappe.tests import IntegrationTestCase
 
 from fossunited.api.checkins import checkin_attendee
-from fossunited.doctype_ids import CHAPTER, CONFERENCE, EVENT, EVENT_TICKET, USER_PROFILE
+from fossunited.doctype_ids import CHAPTER, EVENT, EVENT_TICKET
+from fossunited.tests.utils import insert_test_chapter, insert_test_event
 
 
 class TestFOSSEventTicket(IntegrationTestCase):
     def setUp(self):
-        fake = Faker()
-
-        chapter_member = frappe.db.get_value(USER_PROFILE, {"user": "test1@example.com"}, ["name"])
-        self.member_email = "test1@example.com"
-        self.member_profile = chapter_member
-
-        if not frappe.db.exists("Role", "Chapter Team Member"):
-            frappe.get_doc({"doctype": "Role", "role_name": "Chapter Team Member"}).insert(
-                ignore_permissions=True
-            )
-        if not frappe.db.exists("Role", "Chapter Lead"):
-            frappe.get_doc({"doctype": "Role", "role_name": "Chapter Lead"}).insert(
-                ignore_permissions=True
-            )
-
-        chapter = frappe.get_doc(
-            {
-                "doctype": CHAPTER,
-                "chapter_type": CONFERENCE,
-                "chapter_name": fake.name(),
-                "email": fake.email(),
-                "about_chapter": fake.text(),
-                "chapter_members": [{"chapter_member": chapter_member}],
-            }
+        self.lead_email = "test1@example.com"
+        self.chapter = insert_test_chapter(lead_email=self.lead_email)
+        self.event = insert_test_event(
+            chapter=self.chapter, is_paid_event=True, tickets_status="Live"
         )
-        chapter.insert(ignore_permissions=True)
-
-        event = frappe.get_doc(
-            {
-                "doctype": EVENT,
-                "chapter": chapter.name,
-                "event_name": fake.name(),
-                "event_permalink": fake.slug().replace("-", "_"),
-                "status": "Live",
-                "event_type": CONFERENCE,
-                "event_start_date": datetime.today(),
-                "event_end_date": datetime.today() + timedelta(1),
-                "event_description": fake.text(),
-                "is_paid_event": 1,
-                "tickets_status": "Live",
-                "tiers": [
-                    {
-                        "enabled": 1,
-                        "title": "General",
-                        "price": 100,
-                    }
-                ],
-            }
-        )
-        event.insert(ignore_permissions=True)
-
-        self.event = event
 
     def tearDown(self):
         frappe.set_user("Administrator")
         frappe.delete_doc(EVENT, self.event.name, force=True)
+        frappe.delete_doc(CHAPTER, self.chapter.name, force=True)
 
     def test_checkin(self):
         fake = Faker()
@@ -86,12 +36,14 @@ class TestFOSSEventTicket(IntegrationTestCase):
         ticket.insert(ignore_permissions=True)
         ticket.reload()
 
+        # There should be no check-ins for that event
         self.assertFalse(ticket.check_ins)
+
         # Set the user as a chapter member
-        frappe.set_user(self.member_email)
+        frappe.set_user(self.lead_email)
 
         # When the user checks in the attendee
-        checkin_attendee(self.event.name, ticket.as_dict(), self.member_email)
+        checkin_attendee(self.event.name, ticket.as_dict(), self.lead_email)
 
         # Then the check-in data should be saved
         ticket.reload()
@@ -100,7 +52,7 @@ class TestFOSSEventTicket(IntegrationTestCase):
         # When the user tries to check-in the attendee again on the same day
         # Then the user should not be able to check-in the attendee again
         with self.assertRaises(frappe.ValidationError):
-            checkin_attendee(self.event.name, ticket.as_dict(), self.member_email)
+            checkin_attendee(self.event.name, ticket.as_dict(), self.lead_email)
 
     def test_checkin_as_non_chapter_member(self):
         fake = Faker()

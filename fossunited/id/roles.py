ac2262211f5915605e3bf3601baa11aa958262fe
harshtandiya <harsh.tandiya@gmail.com>
2025-01-08 15:44:57 +0530

    feat: create `roles` contants file

---
+++ fossunited/id/roles.py
@@ -0,0 +1,12 @@
+# Storing all the Roles for fossunited app in constants
+
+WEBSITE_USER = "FOSS Website User"
+
+CHAPTER_MEMBER = "Chapter Team Member"
+CHAPTER_LEAD = "Chapter Lead"
+
+REVIEWER = "CFP Reviewer"
+
+HACKATHON_ORGANIZER = "Hackathon Organizer"
+
+LOCALHOST_ORGANIZER = "Localhost Organizer"
